f7ef4b3a6eb0dd250305bbefc0392e467dc7b523
shubhamhapse <shapse778@gmail.com>
2017-12-05 08:23:16 +0000

    first commit added readme and c program

---
+++ Example.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("Enter number to be square:");
+	int a;
+	scanf("%d",&a);
+	printf("%d",a*a);
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is project with simple C program to calculate square )-:
